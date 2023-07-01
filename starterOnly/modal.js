e864f6207e7e9a31df96211795e62355201f376f
Mbformation <immvp@hotmail.fr>
2023-07-02 00:22:07 +0200

    Started creation of Form and FormButton objects, deactivate FormButton

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,30 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+class FormTemplate {
+  constructor(formSelector, submitButtonSelector) {
+    this.formSelector = formSelector;
+    this.submitButton = new FormSubmitButton();
+    this.submitButtonSelector = submitButtonSelector;
+  }
+  addFormField() {}
 
+  submitForm() {
+    return this.submitButton.deactivateButton(this.submitButtonSelector);
+  }
+}
+
+class FormSubmitButton {
+  deactivateButton(buttonSelector) {
+    const buttonElement = document.querySelector(buttonSelector);
+    buttonElement.classList.add("disabled");
+  }
+}
+
+class FormField {}
+
+const inscriptionForm = new FormTemplate(
+  ".formData",
+  ".btn-submit"
+).submitForm();
+console.log(inscriptionForm);
